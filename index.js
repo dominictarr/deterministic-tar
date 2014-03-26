2f5206f6a64eaeaf490b156465332251ec2df747
Dominic Tarr <dominic.tarr@gmail.com>
2014-03-26 21:08:28 +1300

    support a mapper to make more changes to the header

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var duplexer = require('duplexer')
 
 var UNIXZERO = new Date(new Date().getTimezoneOffset() * -1)
 
-var deterministic = module.exports = function () {
+var deterministic = module.exports = function (map) {
 
   var pack = tar.pack()
 
   var extract = 
     tar.extract()
       .on('entry', function (header, stream, cb) {
+        if(header.type !== 'file') return cb()
+
         header.mtime = header.atime = header.ctime = UNIXZERO
         header.uid = header.gid = 0
 
@@ -18,11 +20,9 @@ var deterministic = module.exports = function () {
       
         header.mode = header.type === 'directory' ? 0755 : 0644
 
-        if(header.type === 'file') {
-          stream.pipe(pack.entry(header, cb))
-        } else {
-          cb()
-        }
+        if(map) header = map(header) || header
+
+        stream.pipe(pack.entry(header, cb))
       })
       .on('finish', function () {
         pack.finalize()
